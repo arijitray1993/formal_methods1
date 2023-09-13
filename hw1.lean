314846c92fb2dad7b3baa9e7f5d2a2d371bc22a1
Arijit Ray <array@bu.edu>
2023-09-13 18:36:43 -0400

    Update hw1.lean

--- hw1.lean
+++ hw1.lean
@@ -1,7 +1,6 @@
 import Mathlib.Data.Real.Basic
 
---import MIL.Common
-import Mathlib.Data.Real.Basic
+axiom notnotE {p : Prop} (h : ¬ ¬ p) : p
 
 -- slide 21
 theorem slide_21 {p q r : Prop} (h : (p ∧ q) → r) : (p → (q → r))  := by
@@ -11,13 +10,22 @@ theorem slide_21 {p q r : Prop} (h : (p ∧ q) → r) : (p → (q → r))  := by
   apply h h_pq
 
 --slide 23
-theorem slide_23 {p q r : Prop} (h : p → (q → r)) : ((p → q) → (q → r)) := by
+theorem slide_23 {p q r : Prop} (h : p → (q → r)) : (p → q) → (p → r) := by
   intro h_pq
   intro h_p
-  have h_q: q := h_pq h_p
-  have h_qr: q → r := by apply h h_p
-  apply h_qr h_q
+  have h_q := by apply h_pq h_p
+  apply h h_p h_q
 
+--slide 24
+theorem slide_24 {p q r : Prop} (h_1 : p ∧ ¬q → r) (h_2 : ¬r) (h_3 : p) : q := by
+  have notnotq : ¬¬q := by 
+  {
+    intro h_notq
+    have h_pandnotq := by apply And.intro h_3 h_notq
+    have h_r : r := by apply h_1 h_pandnotq
+    contradiction
+  }
+  apply notnotE notnotq
 
 -- example 1.3.1
 example {a b : ℤ} (h1 : a = 2 * b + 5) (h2 : b = 3) : a = 11 :=
